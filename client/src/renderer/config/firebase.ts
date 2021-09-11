a8c17cfc127638463886af4a630010b869175500
Dev <devu.nm21@gmail.com>
2021-09-12 00:51:50 +0530

    Add firebase config

---
+++ client/src/renderer/config/firebase.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: 'AIzaSyDNrbhK1K9yYQDFA7IunCbAUJzM8rCFuQc',
+  authDomain: 'labez-f769c.firebaseapp.com',
+  projectId: 'labez-f769c',
+  storageBucket: 'labez-f769c.appspot.com',
+  databaseURL:
+    'https://labez-f769c-default-rtdb.asia-southeast1.firebasedatabase.app',
+  appId: '1:383521533065:web:1c2ff4e18d9a1c795f6a58',
+  measurementId: 'G-Q5GN9HX5YY',
+};
